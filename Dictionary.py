794f601081ba6fdfe39ba66cc5c6da7aa2912b96
Zandeross <aliemresk09@gmail.com>
2021-02-19 17:20:30 +0300

    Create Dictionary.py
    Dictionary Kullanımı

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+Cities= {
+    'Aydın':{
+        'Nufus' : 1098000,
+        'Plaka' : '09',
+        'Bölge' :'Ege'
+    },
+    'İzmir':{
+        'Nufus' : 4321000,
+        'Plaka' : '35',
+        'Bölge' :'Ege'
+    },
+    'İstanbul':{
+        'Nufus' : 15046000,
+        'Plaka' : '34',
+        'Bölge' :'Marmara'
+    }
+}
+print(Cities['İstanbul'])
