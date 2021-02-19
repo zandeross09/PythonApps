2e896d3623d504bbebba7194c3bd592acaa4fe3a
Zandeross <aliemresk09@gmail.com>
2021-02-19 17:45:50 +0300

    Update Dictionary.py
    Role kullanımı eklendi.

--- Dictionary.py
+++ Dictionary.py
@@ -1,18 +1,21 @@
 Cities= {
     'Aydın':{
         'Nufus' : 1098000,
+        'Roles' :['admin','users'],
         'Plaka' : '09',
         'Bölge' :'Ege'
     },
     'İzmir':{
         'Nufus' : 4321000,
+        'Roles' : ['user'],
         'Plaka' : '35',
         'Bölge' :'Ege'
     },
     'İstanbul':{
         'Nufus' : 15046000,
+        'Roles' : ['user'],
         'Plaka' : '34',
         'Bölge' :'Marmara'
     }
 }
-print(Cities['İstanbul'])
+print(Cities['Aydın']['Roles'][0])
